fc5a38e1265b82e60d4fc43863a053b35a4b250b
AramEvin <aramevinyan8@gmail.com>
2023-04-02 00:13:02 +0400

    Add permisions and change it

--- Test2/test1.tf
+++ Test2/test1.tf
@@ -1,6 +1,6 @@
 resource "local_file" "test1" {
     content     = "Test1 add any arguments!"
-    filename = "${path.module}/test.txt"
-    file_permission = 0641
-    directory_permission = 0654
+    filename = "${path.module}/test/test.txt"
+    file_permission = 0777
+    directory_permission = 0751
 } 
