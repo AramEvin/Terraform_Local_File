552f00975c84030e53288facbeb259990ef89b76
AramEvin <aramevinyan8@gmail.com>
2023-04-01 23:49:49 +0400

    Adding comments

--- Test1/test.tf
+++ Test1/test.tf
@@ -1,4 +1,5 @@
-resource "local_file" "tets" {
-  content = "Hello From Terraform !"
-  filename = "./test.txt"
+#Add resource local_file and add content in test.txt file
+resource "local_file" "test" { # local_file is resource , test is name file to apply in terraform
+  content = "Hello From Terraform !" #content in file 
+  filename = "./test.txt" #path file or directory 
 }
